99c2f3c5e7a6489274a61872402f59f0b67e1edc
Austin East <austineast.dev@gmail.com>
2026-02-19 11:34:07 -0800

    Cursor Behavior - add toggle methods

--- Runtime/Scriptable Object Behaviors/CursorBehavior.cs
+++ Runtime/Scriptable Object Behaviors/CursorBehavior.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Cursor Behavior_ New", menuName = "Behaviors/Application/Cursor Behavior")]
 public class CursorBehavior : ScriptableObject
 {
-	public void SetCursorLockState(CursorLockMode cursorLockMode) => Cursor.lockState = cursorLockMode;
-	public void SetCursorLockStateNone() => SetCursorLockState(CursorLockMode.None);
-	public void SetCursorLockStateConfined() => SetCursorLockState(CursorLockMode.Confined);
-	public void SetCursorLockStateLocked() => SetCursorLockState(CursorLockMode.Locked);
+    public void SetCursorLockState(CursorLockMode cursorLockMode) => Cursor.lockState = cursorLockMode;
+    public void SetCursorLockStateNone() => SetCursorLockState(CursorLockMode.None);
+    public void SetCursorLockStateConfined() => SetCursorLockState(CursorLockMode.Confined);
+    public void SetCursorLockStateLocked() => SetCursorLockState(CursorLockMode.Locked);
+
+    public void ToggleCursorStateConfined() => SetCursorLockState(Cursor.lockState == CursorLockMode.Confined ? CursorLockMode.None : CursorLockMode.Confined);
+    public void ToggleCursorStateLocked() => SetCursorLockState(Cursor.lockState == CursorLockMode.Locked ? CursorLockMode.None : CursorLockMode.Locked);
 }
